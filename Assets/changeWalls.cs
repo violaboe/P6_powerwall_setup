421420fa5f84546d4822083720e6464253c51849
violaboe <viola@repplinger.com>
2024-06-06 09:16:38 +0200

    demo scene

--- Assets/changeWalls.cs
+++ Assets/changeWalls.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class changeWalls : MonoBehaviour
@@ -7,11 +8,13 @@ public class changeWalls : MonoBehaviour
     public Material neon;
     public roomManager roomManager;
     private List<GameObject> walls = new List<GameObject>();
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
+
     public void SceneLoaded()
     {
         walls = roomManager.GetAllWalls();
@@ -21,12 +24,25 @@ public class changeWalls : MonoBehaviour
     {
         if (other.name == "WALL_FACE_EffectMesh")
         {
+<<<<<<< Updated upstream
           //  other.gameObject.GetComponent<MeshRenderer>().material.color = ;
+=======
+            // Set the material to neon
+            other.gameObject.GetComponent<MeshRenderer>().material = neon;
+
+            // Get a random color and set its alpha to 0.2
+            Color randomColor = Random.ColorHSV();
+            randomColor.a = 0.6f;
+
+            // Apply the color with the updated alpha to the material
+            other.gameObject.GetComponent<MeshRenderer>().material.color = randomColor;
+>>>>>>> Stashed changes
         }
     }
+
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
