e15ab44c20936ef39e6726625db8664f84a2fe4b
Khademhaftani <mohammadali.khademhaftani@stud.h-da.de>
2024-06-26 19:15:16 +0200

    New Alignment System - There is still one bug that needs to be fixed. But overall is working

--- Assets/MoeNetworking/camera_position_sending.cs
+++ Assets/MoeNetworking/camera_position_sending.cs
@@ -23,6 +23,13 @@ public class camera_position_sending : NetworkBehaviour
 
     public TimeLineStarter timeLineStarter;
 
+    public Vector3 vectorToScreenMultiplayer;
+    public Vector3 planNormalMultiplayer;
+
+    public AlignmentControllerQuest alignmentControllerQuest;
+    public AlignmentControllerCave alignmentControllerCave;
+
+
 
 
     private void Start()
@@ -30,40 +37,56 @@ public class camera_position_sending : NetworkBehaviour
         aligmentDebug = GameObject.FindAnyObjectByType<AlignmentDebug>();
 
         timeLineStarter = GameObject.FindAnyObjectByType<TimeLineStarter>();
+
+        alignmentControllerQuest = GameObject.FindAnyObjectByType<AlignmentControllerQuest>();
+        alignmentControllerCave = GameObject.FindAnyObjectByType<AlignmentControllerCave>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(oculus_camera_rig != null && oculus_head_pos_obj != null && oculus_camera_rig.activeInHierarchy)
+
+        if (alignmentControllerQuest.isSceneAligned)
         {
-            send_head_pos_to_server(oculus_head_pos_obj.transform.position);
-        }    
+            vectorToScreenMultiplayer = alignmentControllerQuest.VectorToScreen;
+            planNormalMultiplayer = alignmentControllerQuest.planeNormal;
+        }
+        
+
+        if (oculus_camera_rig != null && oculus_head_pos_obj != null && oculus_camera_rig.activeInHierarchy && alignmentControllerQuest.isSceneAligned)
+        {
+            send_head_pos_to_server(vectorToScreenMultiplayer, planNormalMultiplayer);
+        }
     }
 
+
     [ServerRpc(RequireOwnership = false)]
-    private void send_head_pos_to_server(Vector3 _pos)
+    private void send_head_pos_to_server(Vector3 _pos, Vector3 planeNormal)
     {
         Debug.Log("ello");
 
-        send_head_pos_powerwall(_pos);
+        send_head_pos_powerwall(_pos, planeNormal);
 
         Debug.Log("on server: " + _pos);
     }
 
     [ObserversRpc(ExcludeOwner = true)]
-    private void send_head_pos_powerwall(Vector3 _pos)
+    private void send_head_pos_powerwall(Vector3 _pos, Vector3 planeNormal)
     {
-        if(powerwall_cam != null)
-        {
-            if(head_pos.y == 0.0f)
-            {
-                powerwall_cam_offset = _pos;
-            }
-            head_pos = _pos;
-            powerwall_cam.transform.position = _pos - new Vector3(0.0f, 0, 0.5f);
-        }
-        Debug.Log("on client: " + _pos);
+        //if(powerwall_cam != null)
+        //{
+        //    if(head_pos.y == 0.0f)
+        //    {
+        //        powerwall_cam_offset = _pos;
+        //    }
+        //    head_pos = _pos;
+        //    powerwall_cam.transform.position = _pos - new Vector3(0.0f, 0, 0.5f);
+        //}
+        //Debug.Log("on client: " + _pos);
+
+        Debug.Log("on client: " + _pos + "Normal: " + planeNormal);
+        alignmentControllerCave.RepositionCaveCamera(_pos, planeNormal);
+        
     }
 
 
@@ -155,5 +178,6 @@ public class camera_position_sending : NetworkBehaviour
     }
 
 
-
+   
 }
+
