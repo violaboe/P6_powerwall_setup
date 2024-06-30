ec58e097dee50898ea051596b6449232cb48d9d9
violaboe <viola@repplinger.com>
2024-06-30 16:39:46 +0200

    first scene animation

--- Assets/scripts/PlayDirector.cs
+++ Assets/scripts/PlayDirector.cs
@@ -1,18 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.XR;
 
 public class PlayDirector : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private PlayableDirector director;
+    public GameObject controlPanel;
+    public GameObject plants;
+
+    private void Awake()
     {
-        
+        director = GetComponent<PlayableDirector>();
+        director.played += Director_Played;
+        director.stopped += Director_Stopped;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        // Check for the B button press on the Quest 3 controller
+        if (IsBButtonPressed())
+        {
+            StartTimeline();
+            plants.SetActive(true);
+        }
+    }
+
+    private void Director_Stopped(PlayableDirector obj)
+    {
+        controlPanel.SetActive(true);
+    }
+
+    private void Director_Played(PlayableDirector obj)
+    {
+        controlPanel.SetActive(false);
+    }
+
+    public void StartTimeline()
+    {
+        director.Play();
+    }
+
+    private bool IsBButtonPressed()
+    {
+        // Check if the B button is pressed on the right hand controller
+        bool bButtonPressed = false;
+        InputDevice rightHandDevice = InputDevices.GetDeviceAtXRNode(XRNode.RightHand);
+
+        if (rightHandDevice.isValid)
+        {
+            rightHandDevice.TryGetFeatureValue(CommonUsages.secondaryButton, out bButtonPressed);
+        }
+
+        return bButtonPressed;
     }
 }
--- Assets/scripts/Viola/PrefabScaling.cs
+++ Assets/scripts/Viola/PrefabScaling.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class PrefabScaling : MonoBehaviour
@@ -5,18 +6,26 @@ public class PrefabScaling : MonoBehaviour
     public Transform targetTransform; // The transform of the prefab to be scaled
     public Vector3 startScale = Vector3.zero; // Starting scale
     public Vector3 endScale = Vector3.one; // Target scale
+    public bool keepScale;
     private float duration; // Duration of the scaling animation
 
-    void Awake()
+    void Start()
     {
         duration = Random.Range(1f, 2f);
+
         // Ensure the targetTransform is assigned, if not, assign the transform of the GameObject this script is attached to
         if (targetTransform == null)
         {
             targetTransform = transform;
         }
 
+        if (keepScale)
+        {
+            endScale = transform.localScale;
+        }
+
         // Start the scaling animation
         this.Animate(targetTransform, Easing.AnimationType.Scale, Easing.Ease.EaseOutExpo, startScale, endScale, duration);
     }
+
 }
